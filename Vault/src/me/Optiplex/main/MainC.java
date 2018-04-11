599a17c90149177a68b1edfe0a64a28341086e43
TheBeast572 <thebeast572323@gmail.com>
2018-04-10 22:24:06 -0700

    Added some shit to Main
    :D

--- Vault/src/me/Optiplex/main/MainC.java
+++ Vault/src/me/Optiplex/main/MainC.java
@@ -4,6 +4,18 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MainC extends JavaPlugin{
+	Plugin plugin;
 	Player p ;
 	String s "BEAST WAS HERE";
+	
+	public void onEnable() {
+		plugin = this;
+	}
+	
+	public void onDisable() {
+		plugin = null;
+	}
+	public Plugin getPlugin() {
+		return plugin;
+	}
 }
